2e12667a27c69519798bf8ac9fc49ef13b761bb2
ATUL <atulmint1502@gmail.com>
2026-01-28 18:36:01 +0530

    hooks usestate

--- 05_talwind_css/src/App.jsx
+++ 05_talwind_css/src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import section1 from './componets/section1/section1'
+import React, { useState } from 'react'
 
 const App = () => {
+
+const [num, setnum] = useState(0)
+
+function increasenums(){
+   setnum(num+1)
+}
+
+function decreasenum(){
+console.log('decrease');
+setnum(num-1)
+}
+
+function jumpby5(){
+console.log('jumpby5');
+setnum(num+5)
+}
+
   return (
     <div>
-     
-     </div>
+      <h1>{num}</h1>
+      <button onClick={increasenums}>increase</button>
+      <button onClick={decreasenum}>decrease</button>
+       <button onClick={jumpby5}>jumpby5</button>
+    </div>
   )
 }
 

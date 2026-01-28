1fe52941702e41c95a185691a106ae389fa61e01
ATUL <atulmint1502@gmail.com>
2026-01-28 18:35:46 +0530

    use state

---
+++ 05_talwind_css/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import section1 from './componets/section1/section1'
+
+const App = () => {
+  return (
+    <div>
+     
+     </div>
+  )
+}
+
+export default App
---
+++ 05_talwind_css/src/componets/section1/section1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const section1 = () => {
+  return (
+    <div className='h-screen w-full bg-amber-300'>Section1</div>
+  )
+}
+
+export default section1

a4e9eb6ade5afa6c5015d2611f5bdd36b30ac7e5
ATUL <atulmint1502@gmail.com>
2026-01-29 10:55:45 +0530

    complete form handling

--- 06_formhandling/src/App.jsx
+++ 06_formhandling/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const App = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('Form Submitted');
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit = {(e) => {
+        submitHandler(e)
+      }}>
         <input type='text' placeholder='Enter your name' />
         <button>Submit</button>
       </form>

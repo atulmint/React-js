a36afc62c8541bb05e05f0eb5c31b71ba607ca7d
ATUL <atulmint1502@gmail.com>
2026-01-29 10:45:12 +0530

    form handling

---
+++ 06_formhandling/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type='text' placeholder='Enter your name' />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
